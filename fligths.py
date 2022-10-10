2da7af162e40205eff1abcbcd5be508e6b558798
Maciej Olszanski <m.olszanski@accenture.com>
2022-10-10 14:31:50 +0200

    first commit

---
+++ fligths.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+import datetime
+
+
+ARRIVAL_LATEST = datetime.date(2023, 2, 3)
+RETURN_EARLIEST = datetime.date(2023, 2, 6)
+
+print(ARRIVAL_LATEST)
+print(RETURN_EARLIEST)
