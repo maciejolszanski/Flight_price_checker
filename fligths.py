b80c9cace211a6ba114814cbe5b2ece96aa741e6
Maciej Olszanski <m.olszanski@accenture.com>
2022-10-11 15:49:54 +0200

    flights.py: reading flight params & saving to dict

--- fligths.py
+++ fligths.py
@@ -1,6 +1,5 @@
-from bs4 import BeautifulSoup
+import re
 import datetime
-import requests
 from time import sleep
 
 from selenium import webdriver
@@ -15,7 +14,7 @@ SKYSCANNER_BASE_URL = f"https://www.google.com/travel/flights"
 
 
 
-def format_date_url(date):
+def format_date(date):
 
     formatted_date = '.'.join(str(date).split('-'))
 
@@ -35,12 +34,12 @@ def input_dates(dep_date, ret_date):
     # departure date form                          
     dep_date_form = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')
     dep_date_form.clear()                          
-    dep_date_form.send_keys(format_date_url(dep_date), Keys.ENTER)
+    dep_date_form.send_keys(format_date(dep_date), Keys.ENTER)
 
     # return date form 
     ret_date_form = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/input')
     ret_date_form.clear()
-    ret_date_form.send_keys(format_date_url(ret_date), Keys.ENTER)
+    ret_date_form.send_keys(format_date(ret_date), Keys.ENTER)
 
     # Stop is needed, because sometimes the dates are not being updated until the search button is clicked and then the dates are wrong
     sleep(1)
@@ -86,7 +85,76 @@ if __name__ == "__main__":
     # Sometimes the ID changes so this does not work always
     # choose_num_stops(0)
 
-    all_results = driver.find_elements(By.CLASS_NAME, 'pIav2d')
+    all_results = driver.find_elements(By.CLASS_NAME, 'JMc5Xc')
     print("Number of results: ", len(all_results))
-    print(all_results)
 
+    list_of_dicts = []
+
+    for elem in all_results:
+        text = elem.get_attribute('aria-label')
+
+        price = re.findall(r"From \d+", text)[0].split(' ')[1]
+
+        nonstop = re.findall(r"\bNon", text)
+        if len(nonstop) > 0:
+            stops = 0
+            stop_time = None
+            stop_city = None
+        else:
+            stops = re.findall(r"\d+ stop", text)[0].split(' ')[0]
+            stop_data = re.findall(r'((\d+ hr)? (\d+ min)?)+ (overnight )?layover at (\S+)', text)
+            stop_time = stop_data[0][0].strip()
+            stop_city = stop_data[0][4].strip()
+
+        airline = ' '.join(re.findall(r"((with \S+) (\S+\.)?)", text)[0][0].split(' ')[1:])
+        airline = airline.rstrip()[:-1] # removing dot
+
+        times = re.findall(r"\d+:\d+ [APM]+", text)
+        dep_time = times[0]
+        arr_time = times[1]
+
+        dates = re.findall(r"\S+, \S+ \d", text)
+        dep_date = dates[0] + " 2023"
+        arr_date = dates[1] + " 2023"
+
+        duration = (re.findall(r"((duration \d+ hr\.?)+ (\d+ min)?)", text)[0][0])
+        duration = ' '.join(duration.split(' ')[1:]).rstrip()
+        if duration[-1] == '.':
+            duration = duration[:-1]       
+        
+        today = format_date(datetime.date.today())
+        
+        elem_dict = {
+            "date_readed": today,
+            "departure_date": dep_date,
+            "arrival_date": arr_date,
+            "return_date": format_date(RETURN_EARLIEST),
+            "price": int(price),
+            "airline": airline,
+            "duration": duration,
+            "stops": {
+                "stops_num": stops,
+                "place": stop_city,
+                "stop_duration": stop_time,
+            }
+        }
+
+        list_of_dicts.append(elem_dict)
+
+    driver.close()
+
+"""
+From 608 Polish zlotys round trip total. 
+1 stop flight with Lufthansa. 
+Leaves John Paul II Kraków-Balice International Airport at 1:25 PM on Friday, February 3 
+and arrives at Josep Tarradellas Barcelona-El Prat Airport at 5:35 PM on Friday, February 3.
+Total duration 4 hr 10 min. 
+Layover (1 of 1) is a 50 min layover at Munich International Airport in Munich. 
+Select flight
+
+From 1158 Polish zlotys round trip total.
+This price does not include overhead bin access.
+Nonstop flight with Wizz Air.
+Leaves John Paul II Kraków-Balice International Airport at 6:45 AM on Friday, February 3
+and arrives at Josep Tarradellas Barcelona-El Prat Airport at 9:30 AM on Friday, February 3
+"""
