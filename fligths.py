efed6513198ba90cf973fd1c6adf5c0c6e2d44f5
Maciej Olszanski <olszanskimaciej@wp.pl>
2022-10-12 12:14:31 +0200

    add checking all possible dates

--- fligths.py
+++ fligths.py
@@ -9,10 +9,12 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
 
+DEPART_EARLIEST = datetime.date(2023, 2, 2)
 DEPART_LATEST = datetime.date(2023, 2, 3)
 RETURN_EARLIEST = datetime.date(2023, 2, 6)
+RETURN_LASTEST = datetime.date(2023, 2, 8)
 DESTINATION = "Barcelona"
-SKYSCANNER_BASE_URL = f"https://www.google.com/travel/flights"
+GOOGLE_BASE_URL = f"https://www.google.com/travel/flights"
 
 
 
@@ -21,6 +23,25 @@ def format_date(date):
     formatted_date = '.'.join(str(date).split('-'))
 
     return formatted_date
+
+def find_all_dates(dep_earl, dep_lat, ret_earl, ret_lat):
+
+    dep = dep_earl
+    ret = ret_earl
+
+    dates_list = []
+
+    while dep <= dep_lat:
+        while ret <= ret_lat:
+
+            dates = (dep, ret)
+            dates_list.append(dates)
+            ret += datetime.timedelta(days=1)
+
+        ret = ret_earl
+        dep += datetime.timedelta(days=1)
+
+    return dates_list
 
 def input_destination(dest):
 
@@ -31,15 +52,17 @@ def input_destination(dest):
     destination_form.clear()
     destination_form.send_keys(dest, Keys.ENTER)
 
+    sleep(1)
+
 def input_dates(dep_date, ret_date):
 
     # departure date form                          
-    dep_date_form = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')
+    dep_date_form = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[1]/div/input')
     dep_date_form.clear()                          
     dep_date_form.send_keys(format_date(dep_date), Keys.ENTER)
 
     # return date form 
-    ret_date_form = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/input')
+    ret_date_form = driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div/div[2]/div[2]/div/div/div[1]/div/div/div[1]/div/div[2]/div/input')
     ret_date_form.clear()
     ret_date_form.send_keys(format_date(ret_date), Keys.ENTER)
 
@@ -69,89 +92,103 @@ def choose_num_stops(num_stops):
     driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div/div[4]/div/div[2]/div[3]/div/div[1]/div[2]/span/button').click()
 
 
+def get_flight_data(elem, ret_date):
+
+    text = elem.get_attribute('aria-label')
+
+    price = re.findall(r"From \d+", text)[0].split(' ')[1]
+
+    nonstop = re.findall(r"\bNon", text)
+    if len(nonstop) > 0:
+        stops = 0
+        stop_time = None
+        stop_city = None
+    else:
+        stops = re.findall(r"\d+ stop", text)[0].split(' ')[0]
+        stop_data = re.findall(r'((\d+ hr)? (\d+ min)?)+ (overnight )?layover at (\S+)', text)
+        stop_time = stop_data[0][0].strip()
+        stop_city = stop_data[0][4].strip()
+
+    airline = ' '.join(re.findall(r"((with \S+) (\S+\.)?)", text)[0][0].split(' ')[1:])
+    airline = airline.rstrip()[:-1] # removing dot
+
+    times = re.findall(r"\d+:\d+ [APM]+", text)
+    dep_time = times[0]
+    arr_time = times[1]
+
+    dates = re.findall(r"\S+, \S+ \d", text)
+    dep_date = dates[0] + " 2023"
+    arr_date = dates[1] + " 2023"
+
+    duration = (re.findall(r"((duration \d+ hr\.?)+ (\d+ min)?)", text)[0][0])
+    duration = ' '.join(duration.split(' ')[1:]).rstrip()
+    if duration[-1] == '.':
+        duration = duration[:-1]       
+    
+    today = format_date(datetime.date.today())
+    
+    elem_dict = {
+        "date_readed": today,
+        "departure_date": dep_date,
+        "arrival_date": arr_date,
+        "departure_time": dep_time,
+        "arrival_time": arr_time,
+        "return_date": format_date(ret_date),
+        "price": int(price),
+        "airline": airline,
+        "duration": duration,
+        "stops": {
+            "stops_num": stops,
+            "place": stop_city,
+            "stop_duration": stop_time,
+        }
+    }
+
+    return elem_dict, int(price)
+
 if __name__ == "__main__":
 
     driver = webdriver.Edge()
     driver.implicitly_wait(3)
-    driver.get(SKYSCANNER_BASE_URL)
+    driver.get(GOOGLE_BASE_URL)
 
     # accepting cookies
     driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button').click()
 
     input_destination(DESTINATION)
-    input_dates(DEPART_LATEST, RETURN_EARLIEST)
 
     # search
     driver.find_element(By.XPATH, '//*[@id="yDmH0d"]/c-wiz[2]/div/div[2]/c-wiz/div[1]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/button').click()
     
-    # Sometimes the ID changes so this does not work always
-    # choose_num_stops(0)
-
-    all_results = driver.find_elements(By.CLASS_NAME, 'JMc5Xc')
-    print("Number of results: ", len(all_results))
-
     filename = "flights_data.json"
+
+    dates = find_all_dates(DEPART_EARLIEST, DEPART_LATEST, RETURN_EARLIEST, RETURN_LASTEST)
+
+    prices = []
 
     with open(filename, 'r') as f:
 
         file_dict = json.load(f)
-        
 
-        for elem in all_results:
-            text = elem.get_attribute('aria-label')
+        for (dep_date, ret_date) in dates:
+            input_dates(dep_date, ret_date)
 
-            price = re.findall(r"From \d+", text)[0].split(' ')[1]
+            # Sometimes the ID changes so this does not work always
+            # choose_num_stops(0)
 
-            nonstop = re.findall(r"\bNon", text)
-            if len(nonstop) > 0:
-                stops = 0
-                stop_time = None
-                stop_city = None
-            else:
-                stops = re.findall(r"\d+ stop", text)[0].split(' ')[0]
-                stop_data = re.findall(r'((\d+ hr)? (\d+ min)?)+ (overnight )?layover at (\S+)', text)
-                stop_time = stop_data[0][0].strip()
-                stop_city = stop_data[0][4].strip()
+            all_results = driver.find_elements(By.CLASS_NAME, 'JMc5Xc')
 
-            airline = ' '.join(re.findall(r"((with \S+) (\S+\.)?)", text)[0][0].split(' ')[1:])
-            airline = airline.rstrip()[:-1] # removing dot
+            for elem in all_results:
+                
+                elem_dict, price = get_flight_data(elem, ret_date)
+                prices.append(price)
 
-            times = re.findall(r"\d+:\d+ [APM]+", text)
-            dep_time = times[0]
-            arr_time = times[1]
+                file_dict["items"].append(elem_dict)
 
-            dates = re.findall(r"\S+, \S+ \d", text)
-            dep_date = dates[0] + " 2023"
-            arr_date = dates[1] + " 2023"
-
-            duration = (re.findall(r"((duration \d+ hr\.?)+ (\d+ min)?)", text)[0][0])
-            duration = ' '.join(duration.split(' ')[1:]).rstrip()
-            if duration[-1] == '.':
-                duration = duration[:-1]       
-            
-            today = format_date(datetime.date.today())
-            
-            elem_dict = {
-                "date_readed": today,
-                "departure_date": dep_date,
-                "arrival_date": arr_date,
-                "return_date": format_date(RETURN_EARLIEST),
-                "price": int(price),
-                "airline": airline,
-                "duration": duration,
-                "stops": {
-                    "stops_num": stops,
-                    "place": stop_city,
-                    "stop_duration": stop_time,
-                }
-            }
-
-            file_dict["items"].append(elem_dict)
-
-        driver.close()
+        # driver.close()
     
     with open(filename, 'w') as f:
-        print(file_dict)
+        # print(file_dict)
         json.dump(file_dict, f)
 
 
